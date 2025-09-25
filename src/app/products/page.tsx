71753908c75f1d1435e5783077a4f050fd4232bb
AbdullohAA <abdullohabduganiev55555@gmail.com>
2025-09-25 13:35:28 +0500

    product fix

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,21 @@
-import { getProducts } from "@/requests";
 import React from "react";
 import Products from "./Products";
+import axiosInstance from "@/axios";
+
+export const getProducts = async () => {
+  try {
+    const res = await axiosInstance.get("/products");
+    console.log(res.data);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export default async function ProductsPage() {
   const prods = await getProducts();
-  <div>rere</div>;
+  if (!prods) {
+    return <div>No products available</div>;
+  }
   return <Products products={prods} />;
 }
--- src/requests/index.ts
+++ src/requests/index.ts
@@ -1,10 +1,5 @@
 import axiosInstance from "@/axios"
 
-export const getProducts = async ()=>{
-    const res = await axiosInstance.get('/products')
-    console.log(res.data);
-    return res.data
-}
 
 
 export const getOrders = async()=>{
